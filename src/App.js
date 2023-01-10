4f6efe942f4d5636a0098c0017a244cb9251a25b
Michał Barczyński <michal.barczynski97@gmail.com>
2023-01-10 23:24:18 +0100

    add all tables and routing to app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import NotFound from './components/pages/NotFound/NotFound';
 import Home from './components/pages/Home/Home';
+import Header from './components/views/Header';
+import Footer from './components/views/Footer';
+import Table from './components/features/Table';
+
 import { Container } from 'react-bootstrap';
-import NavBar from './components/views/NavBar';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <main>
       <Container>
-        <NavBar/>
-        <Home></Home>
-        <NotFound></NotFound>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/table:id' element={<Table/>} />
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+        <Footer/>
       </Container>
     </main>
   );
 }
 
 export default App;
+// useDispatch, useParams, useEffect, selectors w redux
