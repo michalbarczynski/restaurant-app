8897a87334027000f0bb05062ca6a635856534e1
Michał Barczyński <michal.barczynski97@gmail.com>
2023-01-18 12:07:44 +0100

    store and tablesRedux finished

--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -4,12 +4,13 @@ export const getTableByID = ({tables}, tableID) => tables.find(table => table.id
 
 // actions
 const createActionName = actionName => `app/tables/${actionName}`;
-
+const SHOW_TABLES = createActionName('SHOW_TABLES');
 const UPDATE_TABLES = createActionName('UPDATE_TABLES');
 
 // action creators
 
 export const updateTables = payload => ({type: UPDATE_TABLES, payload});
+export const showTables = payload => ({type: SHOW_TABLES, payload});
 
 export const fetchTables = () => {
   return(dispatch) => {
@@ -21,8 +22,11 @@ export const fetchTables = () => {
 
 const tablesRedux = (statePart = [], action) => {
   switch (action.type) {
+    case SHOW_TABLES:
+      console.log([...action.payload]);
+      return [...action.payload];
     case UPDATE_TABLES:
-      return [...action,payload];
+      return statePart.map(table => table.id === action.payload.id ? {...table, ...action.payload} : table);
     default:
       return statePart;
   };
