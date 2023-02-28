c20ab01e9b3d40473ebb1f42b937d1ed284e428e
Michał Barczyński <michal.barczynski97@gmail.com>
2023-02-28 11:21:19 +0100

    add table functionality done, id comparison issue

--- src/components/views/NavBar.js
+++ src/components/views/NavBar.js
@@ -3,11 +3,14 @@ import {Navbar, Container, Nav} from "react-bootstrap";
 
 const NavBar = () => {
     return (
-        <Navbar className="rounded mt-4 mb-4 space-beetween" bg="primary" variant="dark">
+        <Navbar className="rounded mt-4 mb-4y" bg="primary" variant="dark">
             <Container>
                 <Navbar.Brand>Waiter App</Navbar.Brand>
                 <Nav>
                     <Nav.Link href="/">Home</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="/newtable">Add Table</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -1,19 +1,21 @@
 import { API_URL } from "../config";
+import { STATUSES } from "./statusRedux";
 
 // selectors
-export const getAllTableIds = (state) => state.tables.map((table) => table.id);
 export const getAllTables = (state) => state.tables;
-export const getTableByID = ({ tables }, tableId) =>
-  tables.find((table) => table.id === tableId);
+export const getTableByID = ({ tables }, tableId) => tables.find((table) => table.id === tableId);
+export const getAllTableIDs = (state) => state.tables.map(table => table.id);
 
 // actions name
 const createActionName = (actionName) => `app/tables/${actionName}`;
 export const SHOW_TABLES = createActionName("SHOW_TABLES");
 export const UPDATE_TABLE = createActionName("UPDATE_TABLE");
+export const ADD_TABLE = createActionName("ADD_TABLE");
 
 // action creators
 export const showTables = (payload) => ({ type: SHOW_TABLES, payload });
 export const updateTable = (payload) => ({ type: UPDATE_TABLE, payload });
+export const addTable = (payload) => ({ type: ADD_TABLE, payload });
 
 export const fetchTables = () => {
   return (dispatch) => {
@@ -22,6 +24,29 @@ export const fetchTables = () => {
       .then((tables) => dispatch(showTables(tables)));
   };
 };
+
+export const addNewTable = (id) => {
+  return (dispatch) => {
+    const newTable = {
+      id,
+      peopleAmount: 0,
+      maxPeople: 0,
+      bill: 0,
+      status: STATUSES.free
+    }
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTable),
+    };
+    fetch(`${API_URL}/tables`, options)
+    .then(res => res.json())
+    .then((data => dispatch(addTable(data))))
+  }
+}
 
 export const updateTables = (data) => {
   return (dispatch) => {
@@ -33,8 +58,8 @@ export const updateTables = (data) => {
       body: JSON.stringify(data),
     };
     fetch(`${API_URL}/tables/${data.id}`, options)
-      .then((res) => res.json())
-      .then((data) => dispatch(updateTable(data)));
+    .then((res) => res.json())
+    .then((data) => dispatch(updateTable(data)));
   };
 };
 
@@ -47,6 +72,9 @@ const tablesReducer = (statePart = [], action) => {
       return statePart.map((table) =>
         table.id === action.payload.id ? { ...table, ...action.payload } : table
       );
+    case ADD_TABLE:
+      console.log(action.payload, statePart);
+      return [...statePart, {...action.payload}];
     default:
       return statePart;
   }
